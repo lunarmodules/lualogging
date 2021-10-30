d5249260deb97b228d0a192cf12ee7d9e68439db
Thijs Schreijer <thijs@thijsschreijer.nl>
2021-10-30 12:11:53 +0200

    feat(console) option for console logger to write to stderr

--- src/logging/console.lua
+++ src/logging/console.lua
@@ -7,16 +7,31 @@
 --
 -------------------------------------------------------------------------------
 
+local io = require"io"
 local logging = require"logging"
+local prepareLogMsg = logging.prepareLogMsg
+
+local destinations = setmetatable({
+    stdout = "stdout",
+    stderr = "stderr",
+  },
+  {
+    __index = function(self, key)
+      if not key then
+        return "stdout" -- default value
+      end
+      error("destination parameter must be either 'stderr' or 'stdout', got: "..tostring(key), 3)
+    end
+  })
 
 function logging.console(params, ...)
   params = logging.getDeprecatedParams({ "logPattern" }, params, ...)
   local logPattern = params.logPattern
   local timestampPattern = params.timestampPattern
-  -- TODO: add option to log to stderr instead
+  local destination = destinations[params.destination]
 
   return logging.new( function(self, level, message)
-    io.stdout:write(logging.prepareLogMsg(logPattern, timestampPattern, level, message))
+    io[destination]:write(prepareLogMsg(logPattern, timestampPattern, level, message))
     return true
   end)
 end
